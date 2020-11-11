21f4fc6d5de3aeb7ed16f7df100e924fb9747750
Timur <temurt_t@mail.ru>
2020-11-11 20:11:39 +0300

    This is app presents information on sensors

---
+++ App2/SensorsPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App2
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SensorsPage : ContentPage
+    {
+        public SensorsPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void p116_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushModalAsync(new P116_Page());
+        }
+
+        private async void tempSensor_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushModalAsync(new tempSensorPage());
+        }
+    }
+}
