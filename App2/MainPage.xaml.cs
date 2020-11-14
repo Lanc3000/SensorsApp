1475e246ff68e64d0c7e78ba1e8404a03d0da880
Lanc3000 <temurt_t@mail.ru>
2020-11-14 14:39:14 +0300

    Add files via upload

---
+++ App2/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App2
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            
+        }
+
+        private async void gpa12Button_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushModalAsync(new SensorsPage());
+        }
+
+        private void gpa16Button_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void gpa16UButton_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void gpa10Button_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
